8ad87baa873c928f5006632f5a012f81ab5d587d
zihao_il <zihao1220@gmail.com>
2022-07-27 12:04:50 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
-$(document).ready(function () {
-    $(".nav-item").click(function () {
-        if ($(window).width() < 992) {
-            var myCollapse = document.getElementById('site-menu')
-            var bsCollapse = new bootstrap.Collapse(myCollapse, {
-                hide: true
-            });
-        }
-        ;
+$(document).ready(function() {
+	$(".nav-item").click(function() {
+		if ($(window).width() < 992) {
+			var myCollapse = document.getElementById('site-menu')
+			var bsCollapse = new bootstrap.Collapse(myCollapse, {hide: true});
+		};
 
-    });
-
+	});
 });
+
+
