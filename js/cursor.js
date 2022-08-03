f952099046346fcc15e00fa168429f147e0c4e7a
zihao_il <zihao1220@gmail.com>
2022-08-03 19:52:51 +0800

    update

--- js/cursor.js
+++ js/cursor.js
@@ -1,49 +1,29 @@
-window.addEventListener('load', function() {
-	let body = document.body;
-	let content = ["❤要致富❤", "❤先撸树❤", "❤要撸树❤", "❤先种树❤", "❤要种树❤"]
-	body.addEventListener('click', function(e) {
-		let x = e.pageX;
-		let y = e.pageY;
-		let randContent = Math.ceil(Math.random() * content.length);
-		let text = new Text(x, y, randContent);
-		let span = document.createElement('span')
-		span.style.color = text.getRandom();
-		text.create(span);
-		setTimeout(function() {
-			text.out(span)
-		}, 1900)
-	})
+window.onload = function() {
 
-	function Text(x, y, rand) {
-		this.x = x;
-		this.y = y;
-		this.rand = rand;
-	}
-	Text.prototype.create = function(_this) {
-		let body = document.body;
-		_this.innerHTML = content[this.rand - 1];
-		_this.className = 'text'
-		_this.style.top = this.y - 20 + 'px'
-		_this.style.left = this.x - 50 + 'px'
-		_this.style.animation = 'remove 2s'
-		body.appendChild(_this);
-		let i = 0
-		setInterval(() => {
-			_this.style.top = this.y - 20 - i + 'px'
-			i++
-		}, 10);
-	}
-	Text.prototype.out = function(_this) {
-		_this.remove()
-	}
-	Text.prototype.getRandom = function() {
-		let allType = '0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f';
-		let allTypeArr = allType.split(',');
-		let color = '#';
-		for (var i = 0; i < 6; i++) {
-			var random = parseInt(Math.random() * allTypeArr.length);
-			color += allTypeArr[random];
+	var imgsword = [
+		"url(img/cursor/wood_sword.ico), auto",
+		"url(img/cursor/stone_sword.ico), auto",
+		"url(img/cursor/iron_sword.ico), auto",
+		"url(img/cursor/gold_sword.ico), auto",
+		"url(img/cursor/diamond_sword.ico), auto",
+		"url(img/cursor/netherite_sword.ico), auto"
+	];
+	var imgpickaxe = [
+		"url(img/cursor/wood_pickaxe.ico), auto",
+		"url(img/cursor/stone_pickaxe.ico), auto",
+		"url(img/cursor/iron_pickaxe.ico), auto",
+		"url(img/cursor/gold_pickaxe.ico), auto",
+		"url(img/cursor/diamond_pickaxe.ico), auto",
+		"url(img/cursor/netherite_pickaxe.ico), auto"
+	];
+	randomsword = Math.floor((Math.random() * imgsword.length));
+	randompickaxe = Math.floor((Math.random() * imgpickaxe.length));
+	$("body").css("cursor", imgsword[randomsword]);
+	$('a,summary').hover(
+		function() {
+			this.style.cursor = imgpickaxe[randompickaxe];
 		}
-		return color;
-	}
-})
+	);
+
+
+}
--- js/index2.js
+++ js/index2.js
@@ -1,7 +1,5 @@
 // var url="";
-
-window.onload = function() {
-	if ((navigator.userAgent.match(
+if ((navigator.userAgent.match(
 			/(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i))) {
 		url =
 			"https://minecraft.onelink.me/1010960778?pid=minecraftnet&is_retargeting=true&af_dp=minecraft://openStore/?showStoreOffer=";
@@ -10,34 +8,44 @@ window.onload = function() {
 		url = "minecraft://openStore/?showStoreOffer=";
 		// console.log(url);
 	}
+// window.onload = function() {
+// 	if ((navigator.userAgent.match(
+// 			/(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i))) {
+// 		url =
+// 			"https://minecraft.onelink.me/1010960778?pid=minecraftnet&is_retargeting=true&af_dp=minecraft://openStore/?showStoreOffer=";
+// 		// console.log(url);
+// 	} else {
+// 		url = "minecraft://openStore/?showStoreOffer=";
+// 		// console.log(url);
+// 	}
 
-	var imgsword = [
-		"url(img/cursor/wood_sword.ico), auto",
-		"url(img/cursor/stone_sword.ico), auto",
-		"url(img/cursor/iron_sword.ico), auto",
-		"url(img/cursor/gold_sword.ico), auto",
-		"url(img/cursor/diamond_sword.ico), auto",
-		"url(img/cursor/netherite_sword.ico), auto"
-	];
-	var imgpickaxe = [
-		"url(img/cursor/wood_pickaxe.ico), auto",
-		"url(img/cursor/stone_pickaxe.ico), auto",
-		"url(img/cursor/iron_pickaxe.ico), auto",
-		"url(img/cursor/gold_pickaxe.ico), auto",
-		"url(img/cursor/diamond_pickaxe.ico), auto",
-		"url(img/cursor/netherite_pickaxe.ico), auto"
-	];
-	randomsword = Math.floor((Math.random() * imgsword.length));
-	randompickaxe = Math.floor((Math.random() * imgpickaxe.length));
-	$("body").css("cursor", imgsword[randomsword]);
-	$('a,summary').hover(
-		function() {
-			this.style.cursor = imgpickaxe[randompickaxe];
-		}
-	);
+// 	var imgsword = [
+// 		"url(img/cursor/wood_sword.ico), auto",
+// 		"url(img/cursor/stone_sword.ico), auto",
+// 		"url(img/cursor/iron_sword.ico), auto",
+// 		"url(img/cursor/gold_sword.ico), auto",
+// 		"url(img/cursor/diamond_sword.ico), auto",
+// 		"url(img/cursor/netherite_sword.ico), auto"
+// 	];
+// 	var imgpickaxe = [
+// 		"url(img/cursor/wood_pickaxe.ico), auto",
+// 		"url(img/cursor/stone_pickaxe.ico), auto",
+// 		"url(img/cursor/iron_pickaxe.ico), auto",
+// 		"url(img/cursor/gold_pickaxe.ico), auto",
+// 		"url(img/cursor/diamond_pickaxe.ico), auto",
+// 		"url(img/cursor/netherite_pickaxe.ico), auto"
+// 	];
+// 	randomsword = Math.floor((Math.random() * imgsword.length));
+// 	randompickaxe = Math.floor((Math.random() * imgpickaxe.length));
+// 	$("body").css("cursor", imgsword[randomsword]);
+// 	$('a,summary').hover(
+// 		function() {
+// 			this.style.cursor = imgpickaxe[randompickaxe];
+// 		}
+// 	);
 
 
-}
+// }
 
 
 const myskin = [
