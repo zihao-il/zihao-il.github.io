b64a3ed33c460304c25457cbdaf73f6204d24fff
zihao-il <41977021+zihao-il@users.noreply.github.com>
2023-10-05 17:05:55 +0800

    mc233-web
    网站代码
    https://mc233.endyun.ltd/

---
+++ mc233/js/baidu-tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?227f6693cb133fab1ae24dcf19283653";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
